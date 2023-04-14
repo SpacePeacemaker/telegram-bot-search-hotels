20c399150a806d9391ffbd540251e2d3bc6a1c38
SpacePeacemaker <dimbo2007@yandex.ru>
2023-04-14 20:45:46 +0500

    14.04.2023; highprice - 2я попытка

--- handlers/custom_handlers/highprice.py
+++ handlers/custom_handlers/highprice.py
@@ -1,12 +1,12 @@
 import requests
 from config_data import config
 from loader import bot
-from telebot.types import Message
+from telebot.types import Message, CallbackQuery
 from states.search_hotels import HotelInfoState
 import json
 from telebot import types
 from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
-from datetime import date
+from datetime import date, datetime
 
 
 headers = {
@@ -14,6 +14,7 @@ headers = {
     "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
 }
 dict_answer = {}
+check_in = None
 
 
 def get_key(d, user_value):
@@ -69,7 +70,7 @@ def post_request(url, params):
 def highprice(message: Message) -> None:
     bot.set_state(message.from_user.id, HotelInfoState.city, message.chat.id)
     bot.send_message(message.from_user.id, f"{message.from_user.first_name}, сейчас я поищу отели по самой ВЫСОКОЙ "
-                                           f"цене. Сперва мне нужно узнать город, в котором мы будем искать отели. "
+                                           f"цене. Сперва мне нужно узнать МЕСТОПОЛОЖЕНИЕ, в котором мы будем искать отели. "
                                            f"Напишите его.")
 
 
@@ -97,7 +98,7 @@ def get_high_city(message: Message) -> None:
         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=False, one_time_keyboard=True)
         for i_value in dict_answer.values():
             keyboard.add(types.KeyboardButton(text=i_value))
-        bot.send_message(message.chat.id, 'Пожалуйста, уточните желаемое местоположение:', reply_markup=keyboard)
+        bot.send_message(message.chat.id, 'Пожалуйста, уточните желаемое МЕСТОПОЛОЖЕНИЕ:', reply_markup=keyboard)
         bot.set_state(message.from_user.id, HotelInfoState.exact_city, message.chat.id)
 
     else:
@@ -109,27 +110,28 @@ def get_high_city(message: Message) -> None:
 def get_high_exact_city(message: Message) -> None:
     user_id = get_key(dict_answer, message.text)
     bot.send_message(message.from_user.id, "Вы выбрали следующее местоположение - " + message.text)
-    bot.send_message(message.from_user.id, "Отлично! Теперь введите количество отелей, которые вы хотите "
+    bot.send_message(message.from_user.id, "Отлично! Теперь введите КОЛИЧЕСТВО ОТЕЛЕЙ, которые вы хотите "
                                            "посмотреть (не более 10-ти).")
-    bot.set_state(message.from_user.id, HotelInfoState.hotels_number, message.chat.id)
 
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         data["city"] = message.text
         data["city_id"] = user_id
 
-    bot.send_message(message.from_user.id, data["city"], data["city_id"])
+    bot.set_state(message.from_user.id, HotelInfoState.hotels_number, message.chat.id)
+    # bot.send_message(message.from_user.id, data["city"], data["city_id"])
 
 
 @bot.message_handler(state=HotelInfoState.hotels_number)
 def get_high_hotels(message: Message) -> None:
     if message.text.isdigit() and 0 < int(message.text) <= 10:
-        bot.send_message(message.from_user.id, "Отлично! Теперь выберите дату заезда.")
+        bot.send_message(message.from_user.id, "Отлично! Теперь выберите ДАТУ ЗАЕЗДА.")
         bot.set_state(message.from_user.id, HotelInfoState.check_in_date, message.chat.id)
 
         with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data["hotels_number"] = int(message.text)
 
-        bot.send_message(message.from_user.id, data["hotels_number"])
+        calendar_one, step_one = DetailedTelegramCalendar(calendar_id=1, locale="ru", min_date=date.today()).build()
+        bot.send_message(message.from_user.id, f"Выберите {LSTEP[step_one]}", reply_markup=calendar_one)
 
     else:
         bot.send_message(message.from_user.id, "Что-то пошло не так. Либо вы ввели число не от 1 до 10, либо вы ввели "
@@ -137,62 +139,54 @@ def get_high_hotels(message: Message) -> None:
 
 
 @bot.message_handler(state=HotelInfoState.check_in_date)
-def get_high_check_in(message: Message) -> None:
-    bot.send_message(message.chat.id, "Дата заезда:")
-    calendar, step = DetailedTelegramCalendar(calendar_id=1, locale="ru", min_date=date.today()).build()
-    bot.send_message(message.chat.id, f"Выберите {LSTEP[step]}", reply_to_message_id=message.message_id , reply_markup=calendar)
+@bot.callback_query_handler(func=lambda call: call.data)
+def cal_in(call: CallbackQuery):
+    result_one, key_one, step_one = DetailedTelegramCalendar(calendar_id=1, locale="ru", min_date=date.today()).process(call.data)
+    if not result_one and key_one:
+        bot.edit_message_text(f"Выберите {LSTEP[step_one]}",
+                              call.from_user.id,
+                              call.message.message_id,
+                              reply_markup=key_one)
+    elif result_one:
+        bot.edit_message_text(f"Вы выбрали {result_one}",
+                              call.from_user.id,
+                              call.message.message_id)
 
+        check_in = result_one
+        bot.send_message(call.from_user.id, "Отлично! Теперь выберите ДАТУ ВЫЕЗДА.")
+        bot.set_state(call.from_user.id, HotelInfoState.check_out_date, call.message.chat.id)
 
-@bot.callback_query_handler(func=DetailedTelegramCalendar.func(calendar_id=1))
-def cal_in(c):
-    result, key, step = DetailedTelegramCalendar(calendar_id=1, locale="ru", min_date=date.today()).process(c.data)
-    if not result and key:
-        bot.edit_message_text(f"Выберите {LSTEP[step]}",
-                              c.message.chat.id,
-                              c.message.message_id,
-                              reply_markup=key)
-    elif result:
-        bot.edit_message_text(f"Вы выбрали {result}",
-                              c.message.chat.id,
-                              c.message.message_id)
-
-        bot.send_message(c.message.chat.id, "Отлично! Теперь выберите дату выезда.")
-        bot.set_state(c.message.chat.id, HotelInfoState.check_out_date, c.message.chat.id)
-
-        with bot.retrieve_data(c.message.from_user.id, c.message.chat.id) as data:
-            data["check_in_date"] = str(result)   # форма записи: yyyy-mm-dd
-
-        bot.send_message(c.message.from_user.id, data["check_in_date"])
+        with bot.retrieve_data(call.from_user.id, call.message.chat.id) as data:
+            data["check_in_date"] = str(result_one)   # форма записи: yyyy-mm-dd
 
 
 @bot.message_handler(state=HotelInfoState.check_out_date)
-def get_high_check_out(message: Message) -> None:
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        calendar, step = DetailedTelegramCalendar(calendar_id=2, locale="ru", min_date=data["check_in_date"]).build()
-        bot.send_message(message.chat.id, f"Выберите {LSTEP[step]}", reply_markup=calendar)
+@bot.callback_query_handler(func=lambda call: call.data)
+def cal_out(call: CallbackQuery):
+    calendar_two, step_two = DetailedTelegramCalendar(calendar_id=2, locale="ru", min_date=check_in).build()
+    bot.send_message(call.from_user.id, f"Выберите {LSTEP[step_two]}", reply_markup=calendar_two)
 
+    result_two, key_two, step_two = DetailedTelegramCalendar(calendar_id=2, locale="ru").process(call.data)
+    if not result_two and key_two:
+        bot.edit_message_text(f"Выберите {LSTEP[step_two]}",
+                              call.from_user.id,
+                              call.message.message_id,
+                              reply_markup=key_two)
+    elif result_two:
+        bot.edit_message_text(f"Вы выбрали {result_two}",
+                              call.from_user.id,
+                              call.message.message_id)
 
-@bot.callback_query_handler(func=DetailedTelegramCalendar.func(calendar_id=2))
-def cal_out(c):
-    with bot.retrieve_data(c.message.from_user.id, c.message.chat.id) as data:
-        result, key, step = DetailedTelegramCalendar(calendar_id=2, locale="ru", min_date=data["check_in_date"]).process(c.data)
-        if not result and key:
-            bot.edit_message_text(f"Выберите {LSTEP[step]}",
-                                c.message.chat.id,
-                                c.message.message_id,
-                                reply_markup=key)
-        elif result:
-            bot.edit_message_text(f"Вы выбрали {result}",
-                                c.message.chat.id,
-                                c.message.message_id)
+        bot.set_state(call.from_user.id, HotelInfoState.hotel_photos, call.message.chat.id)
 
-            keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=False, one_time_keyboard=True)
-            keyboard.add(types.KeyboardButton(text='Да'))
-            keyboard.add(types.KeyboardButton(text='Нет'))
-            bot.send_message(c.message.chat.id, "Отлично! Теперь подскажите, нужны ли вам фотографии.")
-            bot.set_state(c.message.chat.id, HotelInfoState.hotel_photos, c.message.chat.id)
+        with bot.retrieve_data(call.from_user.id, call.message.chat.id) as data:
+            data["check_out_date"] = str(result_two)   # форма записи: yyyy-mm-dd
 
-            data["check_out_date"] = str(result)   # форма записи: yyyy-mm-dd
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=False, one_time_keyboard=True)
+        keyboard.add(types.KeyboardButton(text='Да'))
+        keyboard.add(types.KeyboardButton(text='Нет'))
+        bot.send_message(call.from_user.id, 'Отлично! Теперь подскажите, нужны ли вам ФОТОГРАФИИ?:', reply_markup=keyboard)
+
 
 
 @bot.message_handler(state=HotelInfoState.hotel_photos)
